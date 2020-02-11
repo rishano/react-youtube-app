2ddfcf2ed0eb819201999dc119503e9ff1b56b26
rishano <rishano__@hotmail.com>
2020-02-11 20:18:07 +0100

    'build'

---
+++ build/precache-manifest.04aff5d1f302f2a32bc6029be9b6f477.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fedc4fd322e605902bf",
+    "url": "/static/css/main.8b61810b.chunk.css"
+  },
+  {
+    "revision": "8fedc4fd322e605902bf",
+    "url": "/static/js/main.8fedc4fd.chunk.js"
+  },
+  {
+    "revision": "945513008b33705d0de2",
+    "url": "/static/js/1.94551300.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7a49176be039f12e1bd4402f819bb9c1",
+    "url": "/index.html"
+  }
+];
